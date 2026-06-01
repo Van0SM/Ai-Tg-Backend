e8afdb92ae84cd93750067378aab952776d7d86d
Van0SM <1999ivan-sm359@mail.ru>
2026-06-01 21:25:15 +0300

    Setup user ORM model. (Also added init file for it)

---
+++ app/models/user.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import BigInteger, String, func
+from datetime import datetime
+
+
+from app.core.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(
+        BigInteger,
+        primary_key=True,
+    )
+    telegram_id: Mapped[int] = mapped_column(
+        BigInteger,
+        unique=True,
+        nullable=False,
+    )
+    username: Mapped[str | None] = mapped_column(
+        String(64),
+        nullable=True,
+    )
+    created_at: Mapped[datetime] = mapped_column(
+        nullable=False,
+        server_default=func.now(),
+    )
