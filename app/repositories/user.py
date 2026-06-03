c1657bc25f631575e9c85c3b71c9f07c20454644
Van0SM <1999ivan-sm359@mail.ru>
2026-06-03 14:35:27 +0300

    User repository exists now. It's even able to do something

---
+++ app/repositories/user.py
@@ -0,0 +1,41 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy import exists, select
+
+
+from app.models.user import User
+
+
+class UserRepository:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def create_user(
+        self,
+        telegram_id: int,
+        username: str | None = None,
+    ) -> User:
+
+        user = User(
+            telegram_id=telegram_id,
+            username=username,
+        )
+
+        self.session.add(user)
+
+        await self.session.flush()
+
+        return user
+
+    async def get_user_by_tg_id(self, telegram_id: int) -> User | None:
+        query = select(User).where(User.telegram_id == telegram_id)
+
+        result = await self.session.execute(query)
+
+        return result.scalar_one_or_none()
+
+    async def exists_by_tg_id(self, telegram_id: int) -> bool:
+        query = select(exists().where(User.telegram_id == telegram_id))
+
+        result = await self.session.execute(query)
+
+        return bool(result.scalar())
