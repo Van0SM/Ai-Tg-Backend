edd3794222832b82de4639c50e39aa7fc31db325
Van0SM <1999ivan-sm359@mail.ru>
2026-06-06 00:58:28 +0300

    Now bot has main function

---
+++ app/bot/main.py
@@ -0,0 +1,21 @@
+import asyncio
+
+from aiogram import Bot, Dispatcher, Router
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from app.core.config import settings
+from app.bot.handlers.start import start_router
+
+bot = Bot(token=settings.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+dp = Dispatcher()
+
+
+async def main():
+    dp.include_router(start_router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
