ce7216e39592436a9a3a1e278df03d9d6befd371
Van0SM <1999ivan-sm359@mail.ru>
2026-06-05 23:55:32 +0300

    Created simple, pretty main keyboard

---
+++ app/bot/keyboards/main_menu.py
@@ -0,0 +1,13 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main_menu = ReplyKeyboardMarkup(
+    keyboard=[
+    [
+        KeyboardButton(text="💬 Новый диалог"),
+        KeyboardButton(text="📜 История"),
+    ],
+    [
+        KeyboardButton(text="⚙️ Настройки"),
+        KeyboardButton(text="ℹ️ Помощь"),
+    ],
+], resize_keyboard=True)
