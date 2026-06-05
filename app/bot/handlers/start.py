f4d8d36eaa52d566afab696f13afc09f7bd69982
Van0SM <1999ivan-sm359@mail.ru>
2026-06-05 22:17:17 +0300

    Added bot starting process. I am new Durov

--- app/bot/handlers/start.py
+++ app/bot/handlers/start.py
@@ -1,9 +1,33 @@
+from app.core.database import async_session_maker
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
+from app.services.user import UserService
+from app.exceptions.user import UserAlreadyExistsError
+
 router = Router()
+
+
 
 @router.message(CommandStart())
 async def command_start_handler(message: Message):
-    await message.answer(text="Hello")
+    user = message.from_user
+
+    if user is None:
+        return 
+
+    async with async_session_maker() as session:
+        user_service = UserService(session)
+        try:
+            await user_service.create_user_if_not_exists(
+                telegram_id=user.id,
+                username=user.username,
+                )
+        except UserAlreadyExistsError:
+            await message.answer(text="Такой пользователь уже существует")
+            return
+
+        await session.commit()
+
+        await message.answer(text="Регистрация успешна")
