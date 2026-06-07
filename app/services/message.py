20232f14e9c61d4181af365c55856d312c799f47
Van0SM <1999ivan-sm359@mail.ru>
2026-06-07 17:15:04 +0300

    added MessageService

---
+++ app/services/message.py
@@ -0,0 +1,35 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.repositories.conversation import ConversationRepository
+from app.repositories.message import MessageRepository
+
+from app.repositories.user import UserRepository
+
+
+class MessageService:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+        self.user_repository = UserRepository(self.session)
+
+    async def save_user_message(self, telegram_id: int, content: str):
+        conversation_repository = ConversationRepository(self.session)
+        message_repository = MessageRepository(self.session)
+
+        user = await self.user_repository.get_user_by_tg_id(telegram_id)
+
+        if user is None:
+            return
+
+        last_conv = await conversation_repository.get_last_conversation(user.id)
+
+        if last_conv is None:
+            return
+
+        new_message = await message_repository.create_message(
+            conversation_id=last_conv.id,
+            role="user",
+            content=content,
+            user_id=user.id,
+        )
+
+        await self.session.commit()
